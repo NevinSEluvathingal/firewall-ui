75f8df5a1d6ec2e278cc6fe3174b5fdd72a13c93
Nevin <nevinsajie@gmail.com>
2024-06-06 17:05:24 +0530

    added routings to different pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
-<script lang="js">
-  import { Button } from "./lib/components/ui/button";
+<script>
+  import { Router, Route } from "svelte-routing";
+
+  import SignIn from "./signIn.svelte";
+  import SignUp from "./signUp.svelte";
+  import bar from "./user.svelte";
 </script>
- 
-<Button href="/dashboard">Dashbrd</Button>
+
+<Router>
+    <Route path="/" component={SignIn} />
+  <Route path="/sign-in" component={SignIn} />
+  <Route path="/sign-up" component={SignUp} />
+  <Route path="/Bar" component={bar} />
+</Router>
