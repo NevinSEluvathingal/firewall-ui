a447b58b6aa994f671625e4c54ba804a79fdff23
Nevin <nevinsajie@gmail.com>
2024-06-17 20:57:49 +0530

    admin page

---
+++ src/admin.svelte
@@ -0,0 +1,48 @@
+<script>
+    import Connected from "$lib/components/ui/hist/hist.svelte"
+    import Hist from "$lib/components/ui/hist/rulez.svelte"
+    import { onMount } from 'svelte';
+   let user=localStorage.getItem('name')
+
+   let username = 'hello';
+
+   onMount(() => {
+        username=user.toUpperCase();
+    });
+
+
+    
+
+</script>
+<style>
+    .bar {
+        width:500px;
+    }
+    .con {
+        width:500px; 
+    }
+    .flex {
+        display:flex;
+        flex-direction:row;
+        height:100vh;
+        width:100vw;
+        align-items:center;
+        justify-content:space-around;
+    }
+    .top {
+        position: absolute;
+        top: 20px;
+        font-size: xx-large;
+        font-weight: bold;
+    }
+</style>
+
+<section class="flex">
+    <div class="bar">
+        <h1 class="top" id="name">Hello {username}</h1>
+        <Hist />
+    </div>
+    <div class="con">
+        <Connected />
+    </div>
+</section>
