ad4677db1cb2198d75a4683b22a44c3713ef1225
Nevin <nevinsajie@gmail.com>
2024-06-18 12:25:35 +0530

    invalid notification page

---
+++ src/lib/components/ui/invalid/invalid.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import {gsap} from 'gsap';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+     gsap.fromTo(
+    '.message',
+    { opacity: 1 },
+    {
+      opacity: 0,
+      duration: 1,
+      delay: 3,
+      onComplete: () => {
+        const mssg = document.querySelector('.message') as HTMLDivElement;
+        if (mssg) {
+          mssg.style.display = 'none';
+        }
+      }
+    }
+  );
+});
+</script>
+
+<div class="message">
+    <h3>invalid credentials</h3>
+</div>
+
+<style>
+    .message {
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        width: 20vw;
+        display: flex;
+        justify-content: center;
+        height: 40px;
+        border-radius: 5px;
+        text-align: center;
+        align-items: center;
+        background-color: black;
+        color: white;
+    }
+</style>
