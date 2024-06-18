2a29819b9b37c8029a7232b1f05e10c67318d98b
Nevin <nevinsajie@gmail.com>
2024-06-18 12:28:56 +0530

    admin page for added rules display

---
+++ src/lib/components/ui/hist/rulez.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<div class="space-y-8">
+    <div><h2>Rules Added</h2></div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">http://hdtoday.cc</p>
+		</div>
+		<div class="ml-auto font-medium">Remove</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">https://helloworld.com</p>
+		</div>
+		<div class="ml-auto font-medium">Remove</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">https://helloworld.com</p>
+		</div>
+		<div class="ml-auto font-medium">Remove</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">https://helloworld.com</p>
+		</div>
+		<div class="ml-auto font-medium">Remove</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">https://helloworld.com</p>
+		</div>
+		<div class="ml-auto font-medium">Remove</div>
+	</div>
+</div>
