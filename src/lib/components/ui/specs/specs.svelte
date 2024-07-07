38eafca72a32bd861b1f2bbec2bd733212f5ece3
Nevin <nevinsajie@gmail.com>
2024-07-07 10:46:42 +0530

    content for showing system specifications

---
+++ src/lib/components/ui/specs/specs.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">   
+</script>
+
+<div class="space-y-8">
+    <div><h2>System Specs</h2></div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">os</p>
+		</div>
+		<div class="ml-auto font-medium">NixOS 24.05</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">kernel</p>
+		</div>
+		<div class="ml-auto font-medium">versio:1-NixOS</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">sysinfo</p>
+		</div>
+		<div class="ml-auto font-medium">version: 1.1.0</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">bios</p>
+		</div>
+		<div class="ml-auto font-medium">vendor: American Megatrends International, LLC.</div>
+	</div>
+	<div class="flex items-center">
+		<div class="ml-4 space-y-1">
+			<p class="text-sm font-medium leading-none">cpu</p>
+		</div>
+		<div class="ml-auto font-medium">GenuineIntel 13th Gen Intel(R)</div>
+	</div>
+</div>
