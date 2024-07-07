c7b412e15e3bfacd769fd3697c323e3a01b18b86
Nevin <nevinsajie@gmail.com>
2024-07-07 10:53:57 +0530

    subpage3 in dashboard

---
+++ src/userstab.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+export { default as  Userstab} from "./userstab.svelte";
+import {Button} from "$lib/registry/button/"
+import {
+		Search,
+	} from "./dashboard/";
+</script>
+<div class="flex items-center space-x-2">
+    <Search />
+    <Button size="sm">
+        Add
+    </Button>
+</div>
