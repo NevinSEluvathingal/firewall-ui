2043c98cffe76a3fd5c9b40d6a5b49f65e1a4ce6
Nevin <nevinsajie@gmail.com>
2024-07-14 09:50:24 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    host:true,
+    port:5173,
+  },
   resolve: {
     alias: {
       $lib: path.resolve("./src/lib"),
